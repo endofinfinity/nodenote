9f3f629bb2ef95580305e0518560f92c3edea308
mailab <z1563492018@gmail.com>
2023-09-15 00:09:56 +0800

    图书类别模块 根据id删除

--- api_server/router/artcate.js
+++ api_server/router/artcate.js
@@ -10,11 +10,16 @@ const artcate_handler = require('../router_handler/artcate')
 const expressJoi = require('@escook/express-joi')
 // 导入文章分类的验证模块
 const { add_cate_schema } = require('../schema/artcate')
+// 导入删除分类的验证规则对象
+const { delete_cate_schema } = require('../schema/artcate')
+
 
 // 获取文章分类的列表数据
 router.get('/cates', artcate_handler.getArticleCates)
 // 新增文章分类的路由
 router.post('/addcates', expressJoi(add_cate_schema), artcate_handler.addArticleCates)
+// 删除文章分类的路由
+router.get('/deletecate/:id', expressJoi(delete_cate_schema),artcate_handler.deleteCateById)
 
 // 向外共享路由对象
 module.exports = router
